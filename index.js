23887f7540491a70e910c71a687d283e8b9856eb
CodingSwordfish <wagobryan@gmail.com>
2024-11-03 13:09:30 +0300

    Add the html boilerplate, add the CSS reset code, and  add the JS file with the Book function constructor

---
+++ index.js
@@ -0,0 +1,21 @@
+let log = console.log;
+
+
+const myLibrary = [];
+
+
+function Book (title,author,pages,read){
+this.title = title;
+this.author = author;
+this.pages = pages;
+this.read = read;
+this.info = function(){
+    return `The ${this.title} by ${this.author},${this.pages} pages, ${this.read}`
+}
+}
+
+
+
+function addBookToLibrary() {
+  // do stuff here
+}
