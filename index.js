1e260d4d03637536e514c9aa03a63e1aff9fae01
CodingSwordfish <wagobryan@gmail.com>
2024-11-03 16:24:20 +0300

    Create the addBookToLibrary function

--- index.js
+++ index.js
@@ -1,39 +1,44 @@
 let log = console.log;
 
-const newBookButton = document.querySelector('.newBookButton');
-const newBookForm = document.getElementById('new-book-form');
-const titleInput = document.getElementById('title');
-const authorInput = document.getElementById('author');
-const pagesInput = document.getElementById('pages');
-const readCheckbox = document.getElementById('read');
-
-newBookButton.addEventListener('click', (e) => {
- 
-  newBookForm.classList.toggle('hidden');
-  
-});
-
-// Initially hide the form
-newBookForm.classList.add('hidden');
-
+const newBookButton = document.querySelector(".newBookButton");
+const newBookForm = document.getElementById("new-book-form");
+const submitButton = document.getElementById("form-submit-button");
+const renderDiv = document.querySelector('.render-div');
 
 
 
 const myLibrary = [];
 
-
-function Book (title,author,pages,read){
-this.title = title;
-this.author = author;
-this.pages = pages;
-this.read = read;
-this.info = function(){
-    return `The ${this.title} by ${this.author},${this.pages} pages, ${this.read}`
-}
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
+function addBookToLibrary(e) {
+  const titleInput = document.getElementById("title").value;
+  const authorInput = document.getElementById("author").value;
+  const pagesInput = document.getElementById("pages").value;
+  const readCheckbox = document.getElementById("read").checked;
 
-
-function addBookToLibrary() {
-  // do stuff here
+  let newBook = new Book(titleInput, authorInput, pagesInput, readCheckbox);
+  myLibrary.push(newBook)
+  render();
+  log(myLibrary);
 }
+
+
+newBookForm.classList.add("hidden");
+
+newBookButton.addEventListener("click", (e) => {
+  newBookForm.classList.toggle("hidden");
+});
+
+submitButton.addEventListener("click", (e)=>{
+  addBookToLibrary()
+  e.preventDefault();
+  newBookForm.classList.toggle("hidden");
+});
+
+
