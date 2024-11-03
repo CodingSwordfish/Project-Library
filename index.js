12056de8659ce1a9c94d70d1bb739cf4123d6849
CodingSwordfish <wagobryan@gmail.com>
2024-11-03 21:50:30 +0300

    Add the cards and their styles

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ let log = console.log;
 const newBookButton = document.querySelector(".newBookButton");
 const newBookForm = document.getElementById("new-book-form");
 const submitButton = document.getElementById("form-submit-button");
-const renderDiv = document.querySelector('.render-div');
-
-
+let renderDivCont = document.querySelector(".renderdiv");
 
 const myLibrary = [];
 
@@ -17,17 +15,54 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(e) {
-  const titleInput = document.getElementById("title").value;
-  const authorInput = document.getElementById("author").value;
-  const pagesInput = document.getElementById("pages").value;
-  const readCheckbox = document.getElementById("read").checked;
+  let titleInputVal = document.getElementById("title").value;
+  let authorInputVal = document.getElementById("author").value;
+  let pagesInputVal = document.getElementById("pages").value;
+  let readCheckboxVal = document.getElementById("read").checked;
 
-  let newBook = new Book(titleInput, authorInput, pagesInput, readCheckbox);
-  myLibrary.push(newBook)
+  let newBook = new Book(
+    titleInputVal,
+    authorInputVal,
+    pagesInputVal,
+    readCheckboxVal
+  );
+  myLibrary.push(newBook);
   render();
-  log(myLibrary);
+  console.log(myLibrary);
 }
 
+function render() {
+  renderDivCont.innerHTML = ""; // Clear existing content
+
+  for (let i = myLibrary.length - 1; i >= 0; i--) {
+    let book = myLibrary[i];
+    let renderCard = document.createElement("div");
+    renderCard.classList.add("renderDiv");
+  
+    const button = `<button class="remButton" onclick="removeBook(${i})">Remove Book</button>`;
+  
+    renderCard.innerHTML = `
+      <h2>Title: ${book.title}</h2>
+      <h3>Author: ${book.author}</h3>
+      <p class="bookPages">Total Pages: ${book.pages}</p>
+      <p class="readOrNot">
+   ${book.read ? `<span class="read">Status: Read</span>` : `<span class="not-read">Status: Not Read</span>`}
+</p>
+      ${button}
+    `;
+
+   
+  
+    renderDivCont.appendChild(renderCard);
+  }
+}
+
+
+
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  render();
+}
 
 newBookForm.classList.add("hidden");
 
@@ -35,10 +70,8 @@ newBookButton.addEventListener("click", (e) => {
   newBookForm.classList.toggle("hidden");
 });
 
-submitButton.addEventListener("click", (e)=>{
-  addBookToLibrary()
+submitButton.addEventListener("click", (e) => {
+  addBookToLibrary();
   e.preventDefault();
   newBookForm.classList.toggle("hidden");
 });
-
-
